49db1a73395f7212e285f94e6b87c5d2a5fed922
Mark_Dolgov <hontonikimoi@gmail.com>
2024-03-11 19:14:41 +0300

    add models, migrations, config admin panel

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import User, Organization, Event
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'phone_number', 'organization',
+                    'is_staff', 'is_active', 'is_superuser')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'organization')
+    search_fields = ('email', 'username', 'phone_number', 'organization__title')
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'address', 'postcode')
+    search_fields = ('title', 'description', 'address', 'postcode')
+
+
+class OrganizationInline(admin.TabularInline):
+    model = Event.organizations.through
+    extra = 0
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    inlines = [OrganizationInline]
+    list_display = ('id', 'title', 'description', 'date', 'display_image')
+    list_filter = ('date', 'organizations')
+    search_fields = ('title', 'description', 'date')
+
+    @staticmethod
+    def display_image(obj) -> str:
+        return obj.image.url if obj.image else 'No image'
+
+    display_image.short_description = 'Image'
--- event/models.py
+++ event/models.py
@@ -1,3 +1,72 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=64, unique=True)
+    phone_number = models.CharField(max_length=16, blank=True, null=True)
+    organization = models.ForeignKey(
+        'Organization', on_delete=models.PROTECT, null=True,
+        blank=True, related_name='users'
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return f'id{self.id} - {self.username}'
+
+
+class Organization(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    address = models.CharField(max_length=256)
+    postcode = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    organizations = models.ManyToManyField(Organization, through='EventOrganization')
+    image = models.ImageField(upload_to='event_images/', null=True, blank=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class EventOrganization(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
